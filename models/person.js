543edfe4b6149993c991534b6434db7724054b5c
Dale <dale@gacusan.com>
2020-11-17 21:25:27 +0800

    Finished Part 3 & Refactored code with AirBnB ESLint Config

--- models/person.js
+++ models/person.js
@@ -3,49 +3,56 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const URL = process.env.MONGO_DB_URL;
 
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        console.log("Successful Connection to MongoDB");
-    }).catch((err) => {
-        console.log("Error Connection to MongoDB:", err.message);
-    });
+mongoose.connect(
+  URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Successful Connection to MongoDB');
+  }).catch((err) => {
+    console.log('Error Connection to MongoDB:', err.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    id: Number,
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true,
-    }
+  id: Number,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
 });
 personSchema.plugin(uniqueValidator);
 
 // This gets executed when the response is received to each object of the array
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        /*
+  transform: (document, returnedObject) => {
+    /*
             ".id" will be a new property of returnedObject
- 
+
             It will hold the value of the current "._id" which is an object
             and transforms it to a string.
- 
+
             "._id" gets deleted but returnedObject has a copy
             of it stored in the ".id" property
- 
+
             From _id: "xxxxxxx"
-            to id: "yyyyyyy"
+            to id: "xxxxxxx"
         */
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+    /* eslint-disable no-param-reassign */
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model('Person', personSchema);
-
--- phonebackend.js
+++ phonebackend.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/person');
+
+const app = express();
 
 /*
     Static Folder is like a "route"
@@ -21,114 +21,109 @@ app.use(cors());
 app.use(express.json());
 
 morgan.token('data', (req, res) => {
-    if (req.method === "POST") {
-        return JSON.stringify(req.body);
-    } else {
-        return null;
-    }
+  if (req.method === 'POST') return JSON.stringify(req.body);
+  return null;
 });
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
-app.get("/", (req, res) => {
-    res.send("Succesfully here!");
+app.get('/', (req, res) => {
+  res.send('Succesfully here!');
 });
 
 // WORKING
-app.get("/info", (req, res) => {
-    Person.find({}).then(people => {
-        res.send(
-            `<div><p>Phonebook has info for <b>${people.length}</b> people</p><p>${new Date()}</p></div>`
-        );
+app.get('/info', (req, res) => {
+  Person.find({}).then((people) => {
+    res.send(
+      `<div><p>Phonebook has info for <b>${people.length}</b> people</p><p>${new Date()}</p></div>`
+    );
+  });
+});
+
+// WORKING
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((result) => {
+    res.json(result);
+  });
+});
+
+// WORKING
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body;
+
+  const newPerson = new Person({
+    name,
+    number,
+  });
+
+  newPerson.save()
+    .then((savedPerson) => {
+      // savedPerson returns the document saved
+      res.json(savedPerson);
+    })
+    .catch((err) => {
+      res.status(409).json(err);
     });
 });
 
 // WORKING
-app.get("/api/persons", (req, res) => {
-    Person.find({}).then(result => {
-        res.json(result);
+app.get('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  // Finds a single document by its _id field
+  // _id = id
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
+});
+
+// WORKING
+app.put('/api/persons/:id', (req, res, next) => {
+  console.log('Did you reach me?');
+
+  const { id, number } = req.body;
+
+  Person.findByIdAndUpdate(id, { number }, { new: true }, (err, doc) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+// WORKING
+app.delete('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      next(err);
     });
-});
-
-// WORKING
-app.post("/api/persons", (req, res) => {
-    const { name, number } = req.body;
-
-    const newPerson = new Person({
-        name,
-        number
-    });
-
-    newPerson.save()
-        .then((savedPerson) => {
-            // savedPerson returns the document saved
-            res.json(savedPerson);
-        })
-        .catch(err => {
-            res.status(409).json(err);
-        });
-
-});
-
-// WORKING
-app.get("/api/persons/:id", (req, res, next) => {
-    const id = req.params.id;
-
-    // Finds a single document by its _id field
-    // _id = id
-    Person.findById(id)
-        .then(person => {
-            if (person) {
-                res.json(person);
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch((err) => next(err));
-});
-
-// WORKING
-app.put("/api/persons/:id", (req, res, next) => {
-    console.log("Did you reach me?");
-
-    const { id, number } = req.body;
-
-    Person.findByIdAndUpdate(id, { number }, { new: true }, (err, doc) => {
-        if (err) {
-            next(err);
-        } else {
-            res.json(doc);
-        }
-    });
-});
-
-// WORKING
-app.delete("/api/persons/:id", (req, res, next) => {
-    const id = req.params.id;
-
-    Person.findByIdAndRemove(id)
-        .then(result => {
-            res.json(result);
-        })
-        .catch(err => {
-            next(err);
-        });
-
 });
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.message);
+  console.error(err.message);
 
-    if (err.name === 'CastError') {
-        return res.status(400).send({ error: 'Malformatted ID' });
-    }
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted ID' });
+  }
 
-    next(err);
-
+  next(err);
 };
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT, () => {
-    console.log(`Server started at Port ${PORT}`);
+  console.log(`Server started at Port ${PORT}`);
 });
